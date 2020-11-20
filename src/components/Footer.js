2824e7dac2fb6cdc630e3b03e4f23d97a3ed29ad
Ernest-hub <55037492+Ernest-hub@users.noreply.github.com>
2020-11-20 14:57:59 -0500

    first commit - Movie app

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Footer() {
+	return (
+		<div>
+			<div className="row">
+				<div className="col-md-12">
+					<div className="footer p-3 mt-4 text-center bg-dark text-light">
+					Developed By : 
+					<span className="text-warning font-weight-normal">
+						 Ernest Korgah
+					<span/>,
+					 Using <i className="fab fa-react" /> React JS &amp; Redux JS integrated with external movies data API
+					 <a href="http://www.ombdapi.com/" target="_blank">
+					 	OMDB
+					 	</a>
+					 </span>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	)
+}
+
+export default Footer;
