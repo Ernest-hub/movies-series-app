7f497fbb079f05cc727a81050629b52e4b9f35d5
Ernest-hub <55037492+Ernest-hub@users.noreply.github.com>
2020-11-20 19:40:13 -0500

    second commit - Movie app

---
+++ src/components/home/Landing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import SearchForm from './SearchForm';
+
+export class Landing extends React.Component {
+	render() {
+		return (
+			<div className="container">
+				<SearchForm />
+				
+			</div>
+		);
+	}
+}
+
+export default Landing
